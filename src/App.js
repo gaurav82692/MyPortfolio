0eaa32a348ad07971ad8087f0869aeef891c957c
gaurav82692 <gaurav82692@gmail.com>
2023-02-24 01:00:06 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import SideBar from './Component/SideBar';
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter,HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <SideBar/>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
